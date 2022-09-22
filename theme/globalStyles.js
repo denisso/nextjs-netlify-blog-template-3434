1f85720c2607b6f5ce48cc42e158785a9375ea80
Denis <like-nix@mail.ru>
2022-09-22 19:46:00 +0300

    add highlight to markdown
    temporary solution

--- theme/globalStyles.js
+++ theme/globalStyles.js
@@ -1,8 +1,10 @@
 import { createGlobalStyle } from "styled-components";
 import { base } from "./styles/base";
 import { variables } from "./styles/variables";
+import highlight from "highlight.js/styles/default.css";
 
 const GlobalStyle = createGlobalStyle`
+    ${highlight}
     ${variables} 
     ${base}`;
 
