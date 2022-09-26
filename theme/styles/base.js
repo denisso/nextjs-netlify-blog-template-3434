dfa97b83fa3e8e187fa9fd2611261d3c9662180a
Denis <like-nix@mail.ru>
2022-09-26 15:29:18 +0300

    mobile friendly

--- theme/styles/base.js
+++ theme/styles/base.js
@@ -57,14 +57,20 @@ export const base = css`
         background-color: ${({ theme }) => theme.palette.rootColor};
         transition: width var(--transition), margin var(--transition),
             border-radius var(--transition);
-        margin: 3rem auto 7rem;
+
         box-shadow: 0 2.8px 2.2px rgba(0, 0, 0, 0.034),
             0 6.7px 5.3px rgba(0, 0, 0, 0.048),
             0 12.5px 10px rgba(0, 0, 0, 0.06),
             0 22.3px 17.9px rgba(0, 0, 0, 0.072),
             0 41.8px 33.4px rgba(0, 0, 0, 0.086),
             0 100px 80px rgba(0, 0, 0, 0.12);
-
+            margin: 3rem auto;
+        ${({ theme }) => theme.breakpoints.down("md")} {
+            margin: 2rem auto;
+        }
+        ${({ theme }) => theme.breakpoints.down("sm1")} {
+            margin: 1rem auto;
+        }
         ${({ theme }) => theme.breakpoints.down("sm")} {
             width: 100%;
             margin: 0 auto 0;
